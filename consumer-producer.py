a94b77eb5f5e4e66ae195b78f46ea86bdb060377
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-10 10:28:00 -0500

    ver2

--- consumer-producer.py
+++ consumer-producer.py
@@ -1,23 +1,25 @@
-import json, time
+mport json, time
 from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-consumer = KafkaConsumer(bootstrap_servers=['ip:8081'],
+consumer = KafkaConsumer(bootstrap_servers=['172.24.41.207:8081'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+consumer.subscribe(pattern='.*.-piso2-.*')
 
-producer = KafkaProducer(bootstrap_servers=['ip:8081'],
+producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-#Modificable
-distancia = 10
+dist = 5
+
 for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
-    if message.value['distancia'] <= distancia :
-        consumer.send('cercanos', {'time': time.strftime("%X"), 'measurement': 'Distance','value': message.value['value'], 'unit': 'km'})
+    if message.value['value'] <= 5:
+        producer.send('promedio', {'time': time.strftime("%X"), 'measurement': 'Sirve', 'value': 1, 'unit': 'Boolean', 'place': 'Current pos'})
         producer.flush()
-    else :
-        producer.send('cercanos', {'mensaje':'No hay parqueaderos cercanos'})
+    else:
+        producer.send('promedio', {'time': time.strftime("%X"), 'measurement': 'Sirve', 'value': 0, 'unit': 'Boolean', 'place': 'Current pos'})
         producer.flush()
+
--- producer.py
+++ producer.py
@@ -3,11 +3,11 @@ from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-producer = KafkaProducer(bootstrap_servers=['ip:8081'],
+producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 while True:
     producer.send('alta-piso2-cuarto1', {'time': time.strftime("%X"), 'measurement': 'Distancia',
                   'value': round(uniform(1, 30),1), 'unit': 'Km', 'place': 'piso2/cuarto1'})
-                  producer.flush()
-                  time.sleep(5)
+    producer.flush()
+    time.sleep(5)
