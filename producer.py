17d75d03b2121a1cce0832bb7653a97137ff17a4
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-10 10:07:23 -0500

    ver1

--- producer.py
+++ producer.py
@@ -3,13 +3,11 @@ from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-producer = KafkaProducer(bootstrap_servers=['ip_kafka_server:kafka_port'], 
-						 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=['ip:8081'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 while True:
-	producer.send('alta-piso2-cuarto1', {'time': time.strftime("%X"), 'measurement': 'temperature', 
-					'value': round(uniform(20, 25),1), 'unit': 'C', 'place': 'piso2/cuarto1'})
-	producer.send('baja-piso2-cuarto2', {'time': time.strftime("%X"), 'measurement': 'temperature', 
-					'value': round(uniform(20, 25),1), 'unit': 'C', 'place': 'piso2/cuarto2'})
-	producer.flush()
-	time.sleep(5)
+    producer.send('alta-piso2-cuarto1', {'time': time.strftime("%X"), 'measurement': 'Distancia',
+                  'value': round(uniform(1, 30),1), 'unit': 'Km', 'place': 'piso2/cuarto1'})
+                  producer.flush()
+                  time.sleep(5)
