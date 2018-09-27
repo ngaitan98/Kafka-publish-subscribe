816fdb421c102b3272df216642548116077ba0b3
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 15:33:38 -0500

    Wtf

--- consumer.py
+++ consumer.py
@@ -26,7 +26,10 @@ def distance(lati1, long1, lati2, long2):
 
 
 for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-    distancia = distance(lat, lon, message.value['latitud'], message.value['longitud']
-    print(distancia)
-                         
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
+    if distance(lat, lon, message.value['latitud'], message.value['longitud']) <= dist:
+        producer.send('Sirven', {'direccion' : message.value['direccion'], 'distancia' : distanica, 'tasaCobro' : message.value['tasaCobro']});
+
+
