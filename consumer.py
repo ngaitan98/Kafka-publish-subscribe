a6c9f3627ecc478b5684265a1b6cda255e376cc5
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 15:08:38 -0500

    Wtf

--- consumer.py
+++ consumer.py
@@ -2,23 +2,21 @@ import json, time
 from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
+import gpxpy.geo
 
 consumer = KafkaConsumer(bootstrap_servers=['172.24.41.207:8081'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-consumer.subscribe(pattern='.*.-piso2-.*')
+consumer.subscribe(pattern='Parqueaderos')
 
 producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
+lat = round(uniform(74047, 74119),1)/1000
+lon = round(uniform(4615, 4701),1)/1000
 dist = 2
 for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
-    if message.value['value'] <= 5:
-        producer.send('promedio', {'time': time.strftime("%X"), 'measurement': 'Sirve', 'value': 1, 'unit': 'Boolean', 'place': 'Current pos'})
-        producer.flush()
-    else:
-        producer.send('promedio', {'time': time.strftime("%X"), 'measurement': 'Sirve', 'value': 0, 'unit': 'Boolean', 'place': 'Current pos'})
-        producer.flush()
+    print(gpxpy.geo.haversine_distance(lat, lon, message.value['latitud'], message.value['longitud']))
+
 
