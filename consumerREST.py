1eba41071e6ec7b7b683769482eade0a380ec3de
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 17:23:19 -0500

    Wtf

--- consumerREST.py
+++ consumerREST.py
@@ -4,14 +4,13 @@ from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-consumer = KafkaConsumer('promedio',
-						 bootstrap_servers=['ip_kafka_server:kafka_port'],
+consumer = KafkaConsumer('Parqueaderos',
+                         bootstrap_servers=['172.24.41.207:8081'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
-
-    r = requests.post("http://ip_RESTservice:service_port/measurements/", data={'measurement': message.value['measurement'], 'value': message.value['value'], 'unit': message.value['unit'], 'place': message.value['place'], 'time': message.value['time']})
-    print (r.status_code, r.reason)
+        r = requests.post("http://172.24.41.208:8082/measurements/", data={'id': message.value['measurement'], 'value': message.value['value'], 'unit': message.value['unit'], 'place': 22})
+        print (r.status_code, r.reason)
--- producer.py
+++ producer.py
@@ -5,16 +5,16 @@ from random import uniform
 
 producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-i = 50
+i = 580
 while i != 0:
-    id = round(uniform(1, 20000),0)
+    id = round(uniform(1, 20000))
     nombre = "El parqueadero aleta" + str(id)
     ciudad = "Bogotá"
     dimensiones = "3x2"
-    lat = round(uniform(74047, 74119),0)/1000
-    lon = round(uniform(4615, 4701),0)/1000
-    id_duenio = round(uniform(1, 20000),0)
-    tasaCobro = round(uniform(70,140),0)
+    lat = round(uniform(74047, 74119))/1000
+    lon = round(uniform(4615, 4701))/1000
+    id_duenio = round(uniform(1, 20000))
+    tasaCobro = round(uniform(70,140))
     producer.send('Parqueaderos', {'id': id , 'nombre': nombre, 'dimesiones' : dimensiones, 'direccion' : 'TODO', 'ocupado' : 0, 'latitud': lat, 'longitud': lon, 'id_duenio': id_duenio, 'tasaCobro' : tasaCobro})
     producer.flush()
     time.sleep(1)
