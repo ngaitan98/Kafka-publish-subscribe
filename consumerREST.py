1c59eac1d1d468a14004c6af3c1792230c1767cf
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 17:34:41 -0500

    Wtf

--- consumerREST.py
+++ consumerREST.py
@@ -12,5 +12,5 @@ for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
-        r = requests.post("http://172.24.41.208:8082/measurements/", data={'id': message.value['measurement'], 'value': message.value['value'], 'unit': message.value['unit'], 'place': 22})
+        r = requests.post("http://172.24.41.208:8082/measurements/", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'ciudad': message.value['ciudad'],'dimesiones' : message.value['dimensiones'], 'direccion' : message.value['direccion'], 'ocupado' : message.value['ocupado'], 'latitud': message.value['latitud'], 'longitud': message.value['longitud'], 'tasaCobro' : message.value['tasaCobro'], 'id_duenio': message.value['id_duenio']})
         print (r.status_code, r.reason)
--- producer.py
+++ producer.py
@@ -11,11 +11,11 @@ while i != 0:
     nombre = "El parqueadero aleta" + str(id)
     ciudad = "Bogotá"
     dimensiones = "3x2"
-    lat = round(uniform(74047, 74119))/1000
-    lon = round(uniform(4615, 4701))/1000
+    lon = round(uniform(74047, 74119))/1000
+    lat = round(uniform(4615, 4701))/1000
     id_duenio = int(round(uniform(1, 20000),0))
     tasaCobro = uniform(70,140)
-    producer.send('Parqueaderos', {'id': id , 'nombre': nombre, 'dimesiones' : dimensiones, 'direccion' : 'TODO', 'ocupado' : 0, 'latitud': lat, 'longitud': lon, 'id_duenio': id_duenio, 'tasaCobro' : tasaCobro})
+    producer.send('Parqueaderos', {'id': id , 'nombre': nombre, 'ciudad': 'Bogotá','dimesiones' : dimensiones, 'direccion' : 'TODO', 'ocupado' : 0, 'latitud': lat, 'longitud': lon, 'id_duenio': id_duenio, 'tasaCobro' : tasaCobro})
     producer.flush()
     time.sleep(1)
     i -= 1
