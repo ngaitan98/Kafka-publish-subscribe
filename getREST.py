452c38196c6300b847061ada009dd00360df065b
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 20:50:37 -0500

    getREST

--- getREST.py
+++ getREST.py
@@ -4,9 +4,10 @@ from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-i = 0
-for message in consumer:
+i,j = 5000,0
+while i != 0:
     r = requests.get("http://172.24.41.208:8082/nidoo/parqueaderolist")
     if r.status_code != 201:
         i += 1
         print(i)
+    i-=1
