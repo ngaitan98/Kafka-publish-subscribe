fc26efc1d2d96573cda688bbd479d6e8dff28a1d
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-28 07:38:27 -0500

    Test

--- producer.py
+++ producer.py
@@ -7,15 +7,15 @@ producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 nombres = ['Juan','Julian','Andres','Nicolas','Manuel','Franciso','Leonel']
 apellidos = ['Gaitan', 'Munios', 'Esobar', 'Bello', 'Lugo', 'Naranjo', 'Alvarez']
-i = 2
-while i != 0:
+i = 5
+while i != 1:
     id = i
     nombre = nombres[int(round(uniform(0,len(nombres) - 1),0))] + ' ' +  apellidos[int(round(uniform(0,len(apellidos) - 1),0))]
     documento = str(i*100)
     usuario = str('.'.join(nombre.split())+str(i))
     contrasenia = str(nombre[0:5] + str(123))
     edad = int(round(uniform(18,60),0))
-    producer.send('Parqueaderos', {'id': 1, 'nombre' : "Nicolas Gaitan", 'documento' : "1018505086", 'usuario' : "n.gaitan", 'contrasenia': "Greenday1", 'tipo': "tipo"})
+    producer.send('Parqueaderos', {'id': i, 'nombre' : nombre, 'documento' : documento, 'usuario' : usuario, 'contrasena': contrasenia, 'edad': edad})
     producer.flush()
     i -= 1
 
