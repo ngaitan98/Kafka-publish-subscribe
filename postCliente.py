ebff86f347b6ed02ecf1accf67c23a462c94762f
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 21:28:55 -0500

    getREST

--- postCliente.py
+++ postCliente.py
@@ -7,9 +7,9 @@ apellidos = ['Gaitan', 'Muñoz', 'Esobar', 'Bello', 'Lugo', 'Naranjo', 'Alvarez'
 while i != 0:
     id = i
     nombre = nombres[int(round(uniform(0,len(nombres) - 1),0))] + apellidos[int(round(uniform(0,len(apellidos) - 1),0))]
-    documento = i*100
+    documento = str(i*100)
     usuario = '.'.join(nombre.split())
     contrasenia = nombre[0:5] + str(123)
     edad = int(round(uniform(18,60),0))
-    r = requests.post("http://172.24.41.208:8082/nidoo/clienteslist", data={'id':id, 'nombre' : nombre, 'documento' : documento, 'usuario' : usuario, 'contrasenia': contrasenia, 'edad': edad})
+    r = requests.post("http://172.24.41.208:8082/nidoo/clientelist", data={'id':id, 'nombre' : nombre, 'documento' : documento, 'usuario' : usuario, 'contrasenia': contrasenia, 'edad': edad})
     i-=1
