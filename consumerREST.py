58cd528fd0efe66a074a7cd57b6ab78debb9abe2
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 19:09:02 -0500

    Wtf

--- consumerREST.py
+++ consumerREST.py
@@ -7,10 +7,10 @@ from random import uniform
 consumer = KafkaConsumer('Parqueaderos',
                          bootstrap_servers=['172.24.41.207:8081'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-
+i = 0
 for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+  
     r = requests.post("http://172.24.41.208:8082/nidoo/parqueaderolist", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'ciudad': message.value['ciudad'],'dimensiones' : message.value['dimensiones'], 'direccion' : message.value['direccion'], 'ocupado' : message.value['ocupado'], 'latitud': message.value['latitud'], 'longitud': message.value['longitud'], 'tasaCobro' : message.value['tasaCobro'], 'id_duenio': message.value['id_duenio']})
-    print (r.status_code, r.reason)
+    if r.status_code != 201:
+        i += 1
+        print(i)
--- producer.py
+++ producer.py
@@ -7,12 +7,12 @@ producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 i = 5000
 while i != 0:
-    id = int(round(uniform(1, 20000),0))
+    id = i
     nombre = "El parqueadero aleta" + str(id)
     dimensiones = "3x2"
     lon = round(uniform(74047, 74119))/1000
     lat = round(uniform(4615, 4701))/1000
-    id_duenio = int(round(uniform(1, 20000),0))
+    id_duenio = i%83
     tasaCobro = uniform(70,140)
     direcciones = ['calle 107a #7c-49', 'carrera 22 # 106b-61', 'caerra 1 # 19A - 22', 'carrera 9 # 22 - 63', 'calle 122 # 23 - 45']
     producer.send('Parqueaderos', {'id': id , 'nombre': nombre, 'ciudad': 'Bogota','dimensiones' : dimensiones, 'direccion' : direcciones[int(round(uniform(0, len(direcciones) - 1),0))], 'ocupado' : 0, 'latitud': lat, 'longitud': lon, 'id_duenio': id_duenio, 'tasaCobro' : tasaCobro})
