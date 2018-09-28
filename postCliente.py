90ce1c1939f2112515bb4bd223efb63d25ef652b
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 21:43:42 -0500

    getREST

--- postCliente.py
+++ postCliente.py
@@ -6,9 +6,9 @@ nombres = ['Juan','Julian','Andres','Nicolas','Manuel','Franciso','Leonel']
 apellidos = ['Gaitan', 'Munios', 'Esobar', 'Bello', 'Lugo', 'Naranjo', 'Alvarez']
 while i != 0:
     id = i
-    nombre = nombres[int(round(uniform(0,len(nombres) - 1),0))] + apellidos[int(round(uniform(0,len(apellidos) - 1),0))]
+    nombre = nombres[int(round(uniform(0,len(nombres) - 1),0))] + ' ' +  apellidos[int(round(uniform(0,len(apellidos) - 1),0))]
     documento = str(i*100)
-    usuario = '.'.join(nombre.split())
+    usuario = '.'.join(nombre.split())+str(i)
     contrasenia = nombre[0:5] + str(123)
     edad = int(round(uniform(18,60),0))
     print(id, nombre, documento, usuario, contrasenia, edad)
