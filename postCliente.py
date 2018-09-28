21fdaa6d0da697611bbaaf85bbce6e3a9f107e42
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-28 08:09:03 -0500

    Test

--- postCliente.py
+++ postCliente.py
@@ -1,14 +1,13 @@
 import requests
 import json, time
 from random import uniform
-from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('Parqueaderos',
-                         bootstrap_servers=['172.24.41.207:8081'],
-                         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-i = 0
-for message in consumer:
-    r = requests.post("http://172.24.41.208:8082/nidoo/dueniolist", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'documento': message.value['documento'],'usuario' : message.value['usuario'], 'contrasena' : message.value['contrasena'], 'tipo' : message.value['tipo']})
+i,j = 0,5000
+inicios['06:00:00','07:00:00','08:00:00','09:00:00','10:00:00', '11:00:00']
+fines['12:00:00','13:00:00','14:00:00','15:00:00','16:00:00', '17:00:00', '18:00:00', '19:00:00', '20:00:00']
+while
+    r = requests.post("http://172.24.41.208:8082/nidoo/reservalist", data={'id': i, 'id_parqueadero': i, 'id_cliente': i,'horaEntrada' : inicios[int(round(uniform(0,len(inicios) - 1),0))], 'HoraSalida' : fines[int(round(uniform(0,len(fines) - 1),0))])
     if r.status_code != 201:
         i += 1
         print(i)
+    j-=1
