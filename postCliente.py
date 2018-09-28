002f49af50f64f175cfe87f25ff3d7db67d32baf
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-28 07:53:09 -0500

    Test

--- postCliente.py
+++ postCliente.py
@@ -8,8 +8,7 @@ consumer = KafkaConsumer('Parqueaderos',
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 i = 0
 for message in consumer:
-    print(message.value)
-    r = requests.post("http://172.24.41.208:8082/nidoo/clientelist", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'documento': message.value['documento'],'usuario' : message.value['usuario'], 'contrasena' : message.value['contrasena'], 'edad' : message.value['edad']})
+    r = requests.post("http://172.24.41.208:8082/nidoo/dueniolist", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'documento': message.value['documento'],'usuario' : message.value['usuario'], 'contrasena' : message.value['contrasena'], 'tipo' : message.value['tipo']})
     if r.status_code != 201:
         i += 1
         print(i)
--- producer.py
+++ producer.py
@@ -5,9 +5,10 @@ from random import uniform
 
 producer = KafkaProducer(bootstrap_servers=['172.24.41.207:8081'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-nombres = ['Juan','Julian','Andres','Nicolas','Manuel','Franciso','Leonel']
-apellidos = ['Gaitan', 'Munios', 'Esobar', 'Bello', 'Lugo', 'Naranjo', 'Alvarez']
+nombres = ['Juan','Julian','Andres','Nicolas','Manuel','Franciso','Leonel', 'Sebastian']
+apellidos = ['Gaitan', 'Munios', 'Esobar', 'Bello', 'Lugo', 'Naranjo', 'Alvarez', 'Sierra', 'Pulido']
 i = 5000
+tipos = ['Empresario', 'Estudiante', 'Duenio Vivienda', 'Dueno Parqueadero']
 while i != 1:
     id = i
     nombre = nombres[int(round(uniform(0,len(nombres) - 1),0))] + ' ' +  apellidos[int(round(uniform(0,len(apellidos) - 1),0))]
@@ -15,7 +16,8 @@ while i != 1:
     usuario = str('.'.join(nombre.split())+str(i))
     edad = int(round(uniform(18,60),0))
     contrasenia = str(nombre[0:5] + str(123))+str(edad%43)+str(usuario[0:2])
-    producer.send('Parqueaderos', {'id': i, 'nombre' : nombre, 'documento' : documento, 'usuario' : usuario, 'contrasena': contrasenia, 'edad': edad})
+    tipo = tipos[int(round(uniform(0,len(tipos) - 1),0))]
+    producer.send('Parqueaderos', {'id': i, 'nombre' : nombre, 'documento' : documento, 'usuario' : usuario, 'contrasena': contrasenia, 'tipo': tipo})
     producer.flush()
     i -= 1
 
