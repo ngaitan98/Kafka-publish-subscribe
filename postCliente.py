6f2dc9b5c4c8aabb427fb1fa306a92e7436e8b0b
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-28 07:29:50 -0500

    Test

--- postCliente.py
+++ postCliente.py
@@ -8,7 +8,7 @@ consumer = KafkaConsumer('Parqueaderos',
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 i = 0
 for message in consumer:
-    r = requests.post("http://172.24.41.208:8082/nidoo/parqueaderolist", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'documento': message.value['documento'],'usuario' : message.value['usuario'], 'contrasenia' : message.value['contrasenia'], 'edad' : message.value['tipo']})
+    r = requests.post("http://172.24.41.208:8082/nidoo/dueniolist", data={'id': message.value['id'] , 'nombre': message.value['nombre'], 'documento': message.value['documento'],'usuario' : message.value['usuario'], 'contrasenia' : message.value['contrasenia'], 'edad' : message.value['tipo']})
     if r.status_code != 201:
         i += 1
         print(i)
