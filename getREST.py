02685089e33899890f232a5825ed8559b2661462
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-09-27 20:53:08 -0500

    getREST

--- getREST.py
+++ getREST.py
@@ -4,9 +4,4 @@ import json, time
 i,j = 5000,0
 while i != 0:
     r = requests.get("http://172.24.41.208:8082/nidoo/parqueaderolist")
-    if r.status_code != 201:
-        i += 1
-        print(i)
-    else:
-        print("Success")
-    i-=1
+    print(r.data)
