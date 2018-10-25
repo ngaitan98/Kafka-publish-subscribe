3b8f506270f7e01439c0a26ca112af07ce1f408d
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-10-25 17:22:57 -0500

    waddap

--- postCliente.py
+++ postCliente.py
@@ -1,38 +1,17 @@
 import requests
-
 import json, time
-
 from random import uniform
-
-i,j = 0,5000
-
-Nombres=['Nicolás','Juan','Manuel','Andrés','Francisco','Leonel','Eldete','Bellosapo','Elchiconavi', 'Elchicogym']
-           
-Apellidos = ['Bello', 'Gaitán', 'Lugo', 'Escobar', 'Alvarez', 'Puentes', 'Muñoz', 'Espinosa','Elquiebracucas', 'Mashu']
-                        
-while j != 0:
-    r = requests.post("http://34.219.60.225:8000/nidoo/clientelist", data={'nombre':Nombres[int(round(uniform(0,len(Nombres) - 1),0))] + Apellidos[int(round(uniform(0,len(Apellidos) - 1),0))] ,'Documento' :  str(j*1000000), 'Usuario': 'userName'+str(j), 'Contrasena': 'contra'+str(j), 'edad': j%73})
-    if r.status_code != 201:
-        i += 1
-        print(i)
-    print(r)
-    j-=1
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
-                                          
+i = 1
+nombres = ['Juan','Julian','Andres','Nicolas','Manuel','Franciso','Leonel']
+apellidos = ['Gaitan', 'Munios', 'Esobar', 'Bello', 'Lugo', 'Naranjo', 'Alvarez']
+while i != 0:
+    id = i
+    nombre = nombres[int(round(uniform(0,len(nombres) - 1),0))] + ' ' +  apellidos[int(round(uniform(0,len(apellidos) - 1),0))]
+    documento = str(i*100)
+    usuario = '.'.join(nombre.split())+str(i)
+    contrasenia = nombre[0:5] + str(123)
+    edad = int(round(uniform(18,60),0))
+    print(id, nombre, documento, usuario, contrasenia, edad)
+    r = requests.post("http://172.24.41.208:8082/nidoo/clientelist", data={'nombre' : 'Nicolas Gaitan', 'documento' : 1018505086, 'usuario' : 'n.gaitan', 'contrasenia': 'Greenday1', 'edad': 20})
+    i-=1
+    print(r.status_code)
