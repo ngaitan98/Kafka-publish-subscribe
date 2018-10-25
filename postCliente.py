263952a67472c15622bb7ff9cbb96ebd1c5df50a
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-10-25 17:06:41 -0500

    Hola Bello

--- postCliente.py
+++ postCliente.py
@@ -1,14 +1,39 @@
 import requests
+
 import json, time
+
 from random import uniform
 
+
+
 i,j = 0,5000
-inicios = ['06:00:00','07:00:00','08:00:00','09:00:00','10:00:00', '11:00:00']
-fines = ['12:00:00','13:00:00','14:00:00','15:00:00','16:00:00', '17:00:00', '18:00:00', '19:00:00', '20:00:00']
-tipos =['Debito', 'Credito', 'Efectivo']
+
+Nombres=['Nicolás','Juan','Manuel','Andrés','Francisco','Leonel','Eldete','Bellosapo','Elchiconavi', 'Elchicogym']
+           
+Apellidos = ['Bello', 'Gaitán', 'Lugo', 'Escobar', 'Alvarez', 'Puentes', 'Muñoz', 'Espinosa','Elquiebracucas', 'Mashu']
+                        
 while j != 0:
-    r = requests.post("http://172.24.41.208:8082/nidoo/transaccionlist", data={'id': j, 'id_reserva': j, 'costo': round(uniform(12000,34000), 2),'tipo' : tipos[int(round(uniform(0,len(tipos) - 1),0))]})
+    r = requests.post("http://34.219.60.225:8000/nidoo/clientelist", data={'nombre':Nombres[int(round(uniform(0,len(Nombres) - 1),0))] + Apellidos[int(round(uniform(0,len(Apellidos) - 1),0))] ,'Documento' :  str(j*1000000), 'Usuario': ‘userName'+str(j), 'Contrasena': 'contra'+str(j), 'edad’: j%73})
     if r.status_code != 201:
         i += 1
         print(i)
     j-=1
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
+                                          
